b8b45460b5a52292ed2a2551210713b60b562108
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-11 01:42:23 +0600

    Update think in Javascript.js

--- think in Javascript.js
+++ think in Javascript.js
@@ -1,1 +1,21 @@
 // think in Javascript
+
+/* recursion function */
+
+// add calculated recursion function
+function sum(i) {
+    if (i == 1) {
+        return 1;
+    }
+    return sum(i - 1) + i;
+}
+// console.log(sum(10));
+
+// factorial calculated recursion function
+function factorial(fact) {
+    if (fact == 1) {
+        return 1;
+    }
+    return factorial(fact - 1) * fact;
+}
+console.log(factorial(5));
