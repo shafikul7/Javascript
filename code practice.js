9a9220f89dcfa882cc500c131ab556c6ee96b860
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-10 19:28:46 +0600

    Update code practice.js

--- code practice.js
+++ code practice.js
@@ -227,3 +227,43 @@ for (let i=0; i < a.length; i++) {
 
 console.log(output);
 
+
+//multi_array_sum
+
+function multi_array_sum(arr) {
+	
+	// see how many things are in the current array
+	var currArrayLength = arr.length;
+	// console.log( "currArrayLength: " + currArrayLength);
+	
+	// loop through the array and sum each element
+	for( var i = 0; i < currArrayLength; i++ ) {
+		
+		// console.log( "arr["+ i +"]: " +  arr[i]);
+		
+		// is the current element a number? if so, add it to the sum
+		if ( typeof arr[i] === "number") {
+			arraySum += arr[i];
+			// console.log( "arraySum: " + arraySum );
+		}
+		// is the current element an array? if so, recursively call this function
+		else if ( Array.isArray( arr[i] ) === true ) {
+			// console.log( "RECURSING");
+			multi_array_sum( arr[i] );
+		}
+		
+		// if the current element is some other thing then it will be ignored
+	}
+
+	return arraySum;
+}
+
+var arraySum = 0;
+var a = [1, [2, [3,4]], [5,6] ];
+var result = multi_array_sum( a );
+console.log( result );
+
+var arraySum = 0;
+var b = [ [3,4,5], 6, 7, 8, [9, [10,11,[12,13]]]];
+var result = multi_array_sum( b );
+console.log( result );
