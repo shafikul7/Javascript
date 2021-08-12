9336ae49465aa8fde9726465b84e1caa26dabd39
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-12 20:41:58 +0600

    Update documentObjectModel.js

--- documentObjectModel.js
+++ documentObjectModel.js
@@ -1,1 +1,17 @@
 // Document Object Model
+
+// Returns a reference to the element by its ID.
+document.getElementById('someid');
+
+// Returns an array-like object of all child elements which have all of the given class names.
+document.getElementsByClassName('someclass');
+
+// Returns an HTMLCollection of elements with the given tag name.
+document.getElementsByTagName('LI');
+
+// Returns the first element within the document that matches the specified group of selectors.
+document.querySelector('.someclass');
+
+// Returns a list of the elements within the document (using depth-first pre-order traversal of the document's nodes)
+// that match the specified group of selectors.
+document.querySelectorAll('div.note, div.alert');
