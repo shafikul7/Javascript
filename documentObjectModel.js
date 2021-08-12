c3d44762f018307460e2427c522760788f0645e5
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-12 20:43:12 +0600

    Update documentObjectModel.js

--- documentObjectModel.js
+++ documentObjectModel.js
@@ -15,3 +15,32 @@ document.querySelector('.someclass');
 // Returns a list of the elements within the document (using depth-first pre-order traversal of the document's nodes)
 // that match the specified group of selectors.
 document.querySelectorAll('div.note, div.alert');
+
+// create new elments
+var newHeading = document.createElement('h1');
+var newParagraph = document.createElement('p');
+
+// create text nodes for new elements
+var h1Text= document.createTextNode('This is a nice header text!');
+var pText= document.createTextNode('This is a nice paragraph text!');
+
+// attach new text nodes to new elements
+newHeading.appendChild(h1Text);
+newParagraph.appendChild(pText);
+
+// elements are now created and ready to be added to the DOM.
+
+// grab element on page you want to add stuff to
+var firstHeading = document.getElementById('firstHeading');
+
+// add both new elements to the page as children to the element we stored in firstHeading.
+firstHeading.appendChild(newHeading);
+firstHeading.appendChild(newParagraph);
+
+// can also insert before like so
+
+// get parent node of firstHeading
+var parent = firstHeading.parentNode;
+
+// insert newHeading before FirstHeading
+parent.insertBefore(newHeading, firstHeading);
