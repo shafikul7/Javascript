4a2658420d228c1a3b81341f0ca76e7618446755
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-25 19:43:14 +0600

    Update code practice.js

--- code practice.js
+++ code practice.js
@@ -267,3 +267,35 @@ var arraySum = 0;
 var b = [ [3,4,5], 6, 7, 8, [9, [10,11,[12,13]]]];
 var result = multi_array_sum( b );
 console.log( result );
+
+// Destucturing
+const fish = {id: 58, name: 'king Hilsha', price: 9000, phone: '0171555555', address: 'chadpur', dress: 'silver' };
+
+const phone = fish.phone;
+const  price = fish.price;
+const dress = fish.dress;
+const id = fish.id;
+
+
+const {phone, price, dress, id} = fish;
+
+console.log(phone);
+
+
+const company = {name: 'GP',
+ ceo: { id: 1, name: 'ajmol', food: 'fuchka',},
+
+ web: {work: 'website development', employee: 22, framework: 'react',
+
+ teach: { first: 'html', second: 'css',  third: 'js'},
+}
+
+};
+
+// const work = company.web.work;
+// const framework = company.web.framework;
+
+const {work, framework} = company.web;
+const {food} = company.ceo;
+const{second, third} = company.web.tech;
+console.log(work, framework, food);
