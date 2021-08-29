caa01b01b68c8359f3d0a8dc4a25cd3ae6145a8f
Md.Shafikul Islam <63946398+mdshafikulislam@users.noreply.github.com>
2021-08-29 21:03:41 +0600

    Update Api.js

--- Api.js
+++ Api.js
@@ -59,4 +59,68 @@
          .then((json) => console.log(json));
  }
 
+// 4th api concept start js
+const kanyeQuoted = () => {
+    fetch('https://api.kanye.rest/')
+        .then(response => response.json())
+        .then(data => displayKane(data))
+}
+
+
+const displayKane = (data) => {
+    const quoteId = document.getElementById('quote');
+    const p = document.createElement('p');
+    p.innerText = data.quote;
+    quoteId.appendChild(p);
+
+
+}
+// 5th api concept start js random
+const buddiesFunction = () => {
+    fetch('https://randomuser.me/api/?results=5')
+        .then(response => response.json())
+        .then(data => randomUser(data))
+}
+
+const randomUser = (data) => {
+    const trversedUsers = data.results;
+    const buddiesId = document.getElementById('buddies');
+    for (const buddie of trversedUsers) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h2> ${buddie.gender}</h2>
+        <h3> ${buddie.name.first}</h3>
+        <p> ${buddie.email}</p>
+        <p> ${buddie.location.street.number}</p>
+        `
+        buddiesId.appendChild(div)
+    }
+}
+
+// 6th api concept start js restCountries
+const restCountriesApi = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => restApiShow(data))
+}
+
+const restApiShow = (data) => {
+    const counterDiv = document.getElementById('restCountries')
+    // for (const counter of data) {
+    //     counterDiv.innerHTML = `
+    //     <h2> ${counter.name}</h2>
+    //     <h3> ${counter.region}</h3>
+    //     <p> ${counter.area}</p>
+    //     `
+    //     console.log(counter.name);
+    // }
+    data.forEach(counter => {
+        counterDiv.innerHTML = `
+            <h2> ${counter.name}</h2>
+            <h3> ${counter.region}</h3>
+            <p> ${counter.area}</p>
+            <img scr="${counter.flags}">
+            `
+    })
+}
  
